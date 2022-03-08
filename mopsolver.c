c11d7eeb210e0ba1cd4b04463f46ee693c273813
dbg3220 <dbg3220@rit.edu>
2022-03-08 13:28:12 -0500

    mopsolver.c created and given a main function with documentation

---
+++ mopsolver.c
@@ -0,0 +1,23 @@
+//
+// File: mopsolver.c
+// Description: source module for solving and pretty-printing mazes
+//
+// @author Damon Gonzalez
+// 
+// @date 3/8/2022
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BLOCKED '#'
+#define PASSABLE '.'
+
+/// Main function for this program. Takes in flag command-line arguments
+///  and solves a maze according to specified parameters.
+///
+/// @param argv  The number of given arguments
+/// @param argc  An array of pointers to each argument
+int main( int argv, char* argc[] ){
+    return EXIT_SUCCESS;
+}
