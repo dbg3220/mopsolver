1bd4afd3c4bb12e8c331b3374ce4b8b2ec1001df
dbg3220 <dbg3220@rit.edu>
2022-03-09 15:47:30 -0500

    Parsed all command line arguments correctly

--- mopsolver.c
+++ mopsolver.c
@@ -9,8 +9,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <getopt.h>
 
-#define BLOCKED '#'
+#include "print_help.h"
+
+#define BLOCKED  '#'
 #define PASSABLE '.'
 
 /// Main function for this program. Takes in flag command-line arguments
@@ -19,5 +23,43 @@
 /// @param argv  The number of given arguments
 /// @param argc  An array of pointers to each argument
 int main( int argv, char* argc[] ){
+    int d, s, p;
+    int opt;
+    while( ( opt = getopt( argv, argc, "hdspi:o:" ) ) != -1 ){
+        switch( opt ){
+            case 'h':
+                print_help_stdout();
+                return EXIT_SUCCESS;
+            case 'd':
+                d = 1;
+                break;
+            case 's':
+                s = 1;
+                break;
+            case 'p':
+                p = 1;
+                break;
+            case 'i':
+                break;
+            case 'o':
+                break;
+            default:
+                print_help_stderr();
+                return EXIT_SUCCESS;
+        }
+    }
+
+    if( d == 1 ){
+        printf( "Display the initial maze\n" );
+    }
+
+    if( s == 1 ){
+        printf( "Print length of shortest path\n" );
+    }
+
+    if( p == 1 ){
+        printf( "Print the path, if it exists\n" );
+    }
     return EXIT_SUCCESS;
 }
+
